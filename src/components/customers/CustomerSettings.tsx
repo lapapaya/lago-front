c55729ae8e00fbee08e3c350b016b25f98f8011d
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-01-04 15:43:15 +0100

    feat: display contact lago modal on premium feature access

--- src/components/customers/CustomerSettings.tsx
+++ src/components/customers/CustomerSettings.tsx
@@ -18,6 +18,8 @@ import {
   EditCustomerVatRateDialog,
   EditCustomerVatRateDialogRef,
 } from '~/components/customers/EditCustomerVatRateDialog'
+import { useIsPremiumUser } from '~/hooks/customer/useIsPremiumUser'
+import { PremiumWarningDialog, PremiumWarningDialogRef } from '~/components/PremiumWarningDialog'
 
 import {
   EditCustomerInvoiceGracePeriodDialog,
@@ -61,6 +63,8 @@ interface CustomerSettingsProps {
 
 export const CustomerSettings = ({ customer }: CustomerSettingsProps) => {
   const { translate } = useInternationalization()
+  const isPremium = useIsPremiumUser()
+
   const { data } = useGetOrganizationSettingsForCustomerQuery()
   const currentOrganization = data?.organization
 
@@ -68,6 +72,7 @@ export const CustomerSettings = ({ customer }: CustomerSettingsProps) => {
   const deleteVatRateDialogRef = useRef<DeleteCustomerVatRateDialogRef>(null)
   const editInvoiceGracePeriodDialogRef = useRef<EditCustomerInvoiceGracePeriodDialogRef>(null)
   const deleteGracePeriodDialogRef = useRef<DeleteCustomerGracePeriodeDialogRef>(null)
+  const premiumWarningDialogRef = useRef<PremiumWarningDialogRef>(null)
 
   return (
     <SideSection>
@@ -145,8 +150,7 @@ export const CustomerSettings = ({ customer }: CustomerSettingsProps) => {
         />
       </InfoBlock>
 
-      {/* TODO - Release when pricing is out
-       <InlineSectionTitle>
+      <InlineSectionTitle>
         <Typography variant="subhead" color="grey700">
           {translate('text_638dff9779fb99299bee912e')}
         </Typography>
@@ -154,8 +158,12 @@ export const CustomerSettings = ({ customer }: CustomerSettingsProps) => {
           <Button
             variant="quaternary"
             size="large"
-            disabled // TODO - change when pricing is set
-            onClick={() => editInvoiceGracePeriodDialogRef?.current?.openDialog()}
+            endIcon={isPremium ? undefined : 'sparkles'}
+            onClick={
+              isPremium
+                ? editInvoiceGracePeriodDialogRef?.current?.openDialog
+                : premiumWarningDialogRef.current?.openDialog
+            }
           >
             {translate('text_638dff9779fb99299bee912a')}
           </Button>
@@ -221,7 +229,7 @@ export const CustomerSettings = ({ customer }: CustomerSettingsProps) => {
               : translate('text_638dff9779fb99299bee9136')
           }
         />
-      </InfoBlock> */}
+      </InfoBlock>
 
       <EditCustomerVatRateDialog ref={editDialogRef} customer={customer} />
       <DeleteCustomerVatRateDialog ref={deleteVatRateDialogRef} customer={customer} />
@@ -231,6 +239,7 @@ export const CustomerSettings = ({ customer }: CustomerSettingsProps) => {
         invoiceGracePeriod={customer.invoiceGracePeriod || 0}
       />
       <DeleteCustomerGracePeriodeDialog ref={deleteGracePeriodDialogRef} customer={customer} />
+      <PremiumWarningDialog ref={premiumWarningDialogRef} />
     </SideSection>
   )
 }
--- src/pages/InvoiceCreditNoteList.tsx
+++ src/pages/InvoiceCreditNoteList.tsx
@@ -4,7 +4,7 @@ import { gql } from '@apollo/client'
 import { useParams, generatePath } from 'react-router-dom'
 
 import { theme, NAV_HEIGHT } from '~/styles'
-import { Typography, ButtonLink } from '~/components/designSystem'
+import { Typography, ButtonLink, Button } from '~/components/designSystem'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
 import {
   useGetInvoiceCreditNotesQuery,
@@ -22,6 +22,8 @@ import {
   VoidCreditNoteDialogRef,
 } from '~/components/customers/creditNotes/VoidCreditNoteDialog'
 import CreditNotesList from '~/components/customers/creditNotes/CreditNotesList'
+import { useIsPremiumUser } from '~/hooks/customer/useIsPremiumUser'
+import { PremiumWarningDialog, PremiumWarningDialogRef } from '~/components/PremiumWarningDialog'
 
 gql`
   query getInvoiceCreditNotes($customerId: ID!, $invoiceId: ID!, $page: Int, $limit: Int) {
@@ -47,7 +49,9 @@ gql`
 const InvoiceCreditNoteList = () => {
   const { invoiceId, id } = useParams()
   const { translate } = useInternationalization()
+  const isPremium = useIsPremiumUser()
   const voidCreditNoteDialogRef = useRef<VoidCreditNoteDialogRef>(null)
+  const premiumWarningDialogRef = useRef<PremiumWarningDialogRef>(null)
   const { data, loading, error, fetchMore } = useGetInvoiceCreditNotesQuery({
     variables: { customerId: id as string, invoiceId: invoiceId as string, limit: 20 },
     skip: !invoiceId || !id,
@@ -59,19 +63,27 @@ const InvoiceCreditNoteList = () => {
       {(!loading || !!creditNotes?.length) && (
         <Header>
           <Typography variant="subhead">{translate('text_636bdef6565341dcb9cfb129')}</Typography>
-          <ButtonLink
-            type="button"
-            disabled={
-              true
-              // TODO: Hidden before liscence release
-              // data?.invoice?.creditableAmountCents === 0 &&
-              // data?.invoice?.refundableAmountCents === 0
-            }
-            buttonProps={{ variant: 'quaternary' }}
-            to={generatePath(CUSTOMER_INVOICE_CREATE_CREDIT_NOTE_ROUTE, { id, invoiceId })}
-          >
-            {translate('text_636bdef6565341dcb9cfb127')}
-          </ButtonLink>
+          {isPremium ? (
+            <ButtonLink
+              type="button"
+              disabled={
+                data?.invoice?.creditableAmountCents === 0 &&
+                data?.invoice?.refundableAmountCents === 0
+              }
+              buttonProps={{ variant: 'quaternary' }}
+              to={generatePath(CUSTOMER_INVOICE_CREATE_CREDIT_NOTE_ROUTE, { id, invoiceId })}
+            >
+              {translate('text_636bdef6565341dcb9cfb127')}
+            </ButtonLink>
+          ) : (
+            <Button
+              variant="quaternary"
+              onClick={premiumWarningDialogRef.current?.openDialog}
+              endIcon="sparkles"
+            >
+              {translate('text_636bdef6565341dcb9cfb127')}
+            </Button>
+          )}
         </Header>
       )}
       <>
@@ -100,6 +112,7 @@ const InvoiceCreditNoteList = () => {
         )}
       </>
       <VoidCreditNoteDialog ref={voidCreditNoteDialogRef} />
+      <PremiumWarningDialog ref={premiumWarningDialogRef} />
     </div>
   )
 }
